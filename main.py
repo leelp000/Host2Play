f67a37b2850ba4ac1d59099d3382be5c47947fb8
leelp000 <858689791@qq.com>
2026-06-10 16:36:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=aa5bed2b-0455-4b5b-8547-b0e5db0d9235",
     # 添加更多链接
 ]
 
